d7b8301d03ae9f7d410e49f7b41c0994cc5c8d99
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-29 21:59:22 +0200

    Add CSS example transitions for navigation menu items

---
+++ html/week-35/transitions/menu.js
@@ -0,0 +1,27 @@
+const listElements = document.querySelectorAll("li");
+const liWithoutClass = Array.from(listElements).filter((li) => !li.id);
+
+const toggleButton = document.querySelector("#toggle-button");
+toggleButton.addEventListener("click", showHide);
+
+console.log(liWithoutClass);
+function showHide() {
+  for (const element of liWithoutClass) {
+    if (element.classList.contains("show")) {
+      element.classList.remove("show");
+      element.classList.add("hide");
+    } else {
+      element.classList.remove("hide");
+      element.classList.add("show");
+    }
+  }
+
+  toggleButton.classList.toggle("flex");
+  // vad händer med knappen i mobil läge?
+
+  if (toggleButton.textContent === "Göm meny") {
+    toggleButton.textContent = "Visa meny";
+  } else {
+    toggleButton.textContent = "Göm meny";
+  }
+}
