3db2d964ff6f689d099e735224cfbba60c0280b4
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-11 11:05:20 +0200

    add input and button element to let user manipulate DOM

--- JavaScript/week-37/dom/app.js
+++ JavaScript/week-37/dom/app.js
@@ -6,13 +6,52 @@ let sortedFrukt = frukter.sort()
 
 const listElement = document.querySelector('#lista');
 
+  sortedFrukt.forEach(frukt => {
+    const li = document.createElement('li');
+    
+    // li.style.color = "orange"
+    li.classList.add('background')
+    li.textContent = frukt
+    
+    listElement.appendChild(li)
+  });
 
-sortedFrukt.forEach(frukt => {
+  const input = document.querySelector('#input');
+  const button = document.querySelector('#button');
+
+button.addEventListener('click', () => {
+  frukter.push(input.value)
+
   const li = document.createElement('li');
-
+    
   // li.style.color = "orange"
   li.classList.add('background')
-  li.textContent = frukt
-
+  li.textContent = input.value
+  
   listElement.appendChild(li)
+
+  console.log(frukter)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// const input = document.querySelector('#input')
+// const button = document.querySelector('#button')
+
+// button.addEventListener('click', () => {
+//   console.log(frukter.push(input.value))
+//   render()
+// })
