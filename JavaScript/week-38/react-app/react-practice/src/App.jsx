4b0365e4e2c6ac15b6b158640c8cbb9c9a6664f0
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-09-18 14:40:35 +0200

    add react practie example

---
+++ JavaScript/week-38/react-app/react-practice/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function Hello() {
+  return (
+    <h1>Hello world!</h1>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+    </>
+  )
+}
+
+export default App
